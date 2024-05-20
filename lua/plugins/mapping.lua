2f4ff74da472e0a290aba0e543d7141305570dbb
Ryan Hung <ryanhungryan@gmail.com>
2024-05-20 11:26:06 +0100

    feat: change option and mapping

--- lua/plugins/mapping.lua
+++ lua/plugins/mapping.lua
@@ -6,6 +6,12 @@ return {
       mappings = {
         n = {
           ["<Leader>e"] = { "<cmd>Telescope file_browser<cr>", desc = "file exploer" },
+          ["<C-a>"] = { "gg<S-v>G", desc = "select all" },
+          ["<tab>"] = { "<cmd>tabnext<cr>", desc = "next tab" },
+          ["<s-tab"] = { "<cmd>tabprev<cr>", desc = "prev tab" },
+        },
+        i = {
+          ["jk"] = { "esc", desc = "change to normal" },
         },
       },
     },
