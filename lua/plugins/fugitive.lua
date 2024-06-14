d27021650a9390c6813fbf132433e22677f7a7cc
Ryan Hung <ryanhungryan@gmail.com>
2024-06-14 15:40:29 +0100

    fix(fugitive.lua): remove fugitive
    remove fugitive as i will use floading termina for git stuff

--- lua/plugins/fugitive.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "tpope/vim-fugitive",
-  keys = {
-    { "<leader>gp", "<cmd>Gpush", desc = "push the reposity" },
-    { "<leader>ga", "<cmd>Gwrite", desc = "add the current file" },
-  },
-}
